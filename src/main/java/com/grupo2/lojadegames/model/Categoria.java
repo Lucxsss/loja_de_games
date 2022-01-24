409a82f6f8da3a81fc2ae34fb71dda5bf3b21cc9
Lucas <watchlucas274@gmail.com>
2022-01-24 14:53:13 -0300

    @Lucxsss[Criando a primeira model]

---
+++ src/main/java/com/grupo2/lojadegames/model/Categoria.java
@@ -0,0 +1,49 @@
+package com.grupo2.lojadegames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String genero;
+	
+	private String produtora;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getProdutora() {
+		return produtora;
+	}
+
+	public void setProdutora(String produtora) {
+		this.produtora = produtora;
+	}
+	
+	
+	
+	
+	
+}
