1b2c36ca5653cea40685cca52bbed521cfbe076c
Evelyn <evelynnunesrosa@gmail.com>
2022-01-24 09:31:22 -0300

    @evyrosa [iniciando projeto]

---
+++ src/test/java/com/grupo2/lojadegames/LojaDeGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo2.lojadegames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaDeGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
