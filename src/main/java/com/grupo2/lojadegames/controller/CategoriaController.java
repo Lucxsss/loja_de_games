02a799daf9afad149de18b65a99995a57374b1a4
Evelyn <evelynnunesrosa@gmail.com>
2022-01-25 17:31:00 -0300

    @evyrosa [ criado método PUT]

--- src/main/java/com/grupo2/lojadegames/controller/CategoriaController.java
+++ src/main/java/com/grupo2/lojadegames/controller/CategoriaController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -58,6 +59,14 @@ public class CategoriaController {
 	public ResponseEntity<Categoria> save(@RequestBody Categoria categoria){
 		return ResponseEntity.status(201).body(repository.save(categoria));
 		
+	}
+	
+	@PutMapping("/alterar/{id}")
+	public ResponseEntity<Categoria> putCategoria(@RequestBody Categoria cat){
+		return repository.findById(cat.getId()).map(resp -> ResponseEntity.status(200).body(repository.save(cat)))
+				.orElseGet(()->{
+					throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "ID não encontrado");
+				});
 	}
 	
 	//DELETE
