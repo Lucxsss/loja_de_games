e3483455c7be188a2f006b51e0e5338576559da7
Rodrigo Baba <rodrigonuciatelli@gmail.com>
2022-01-25 13:29:28 -0300

    @RodrigoBaba [TASK: CRIADO METODO getById]

--- src/main/java/com/grupo2/lojadegames/controller/ProdutoController.java
+++ src/main/java/com/grupo2/lojadegames/controller/ProdutoController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -34,6 +35,12 @@ public class ProdutoController {
 		}
 	}
 	
-	
+	@GetMapping("/id/{id_produto}")
+	public ResponseEntity<Produto> getById (@PathVariable(value = "id_produto") Long id){
+		return repository.findById(id).map(resp -> ResponseEntity.status(200).body(resp))
+				.orElseGet(() -> {
+					throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Id não encontrado!");
+				});
+	}
 	
 }
